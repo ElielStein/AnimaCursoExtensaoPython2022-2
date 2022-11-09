3976c35a27e9268c6b1ca7ff48234b9e0281f161
Eliel Stein <116230396+ElielStein@users.noreply.github.com>
2022-11-09 22:16:44 +0000

    começando a aula de hoje ... laços

--- aula2-2022-10-26c.py
+++ aula2-2022-10-26c.py
@@ -2,8 +2,10 @@ numero = 1
 
 while(numero >= 1 and numero <= 10):
   print(numero)
-  numero+=1 # numero++ = incremento do java
-  # numero = numero + 1
+  numero+=1 
+
+# numero++ = incremento do java          
+# numero = numero + 1
   
 
   
