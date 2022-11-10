7fc0d385e8cff87f7ebe100bcbd1dab9893ff5fa
Eliel Stein <116230396+ElielStein@users.noreply.github.com>
2022-11-10 01:18:28 +0000

    Mostrando dois parâmetros em uma função

--- aula3-2022-11-09b.py
+++ aula3-2022-11-09b.py
@@ -27,4 +27,20 @@ valores = [1.99,24.50,78.27,1515.5]
 
 for valor in valores:
   print(f"O imposto de {valor} é {calcular_imposto(valor)}")
+
+#Declarar um função calcula_imposto_aliquota que recebe dois parâmetros: o preço do produto e a alíquota de imposto a ser aplicada e retorna o imposto calculado. Se a aliquota não for informada, utilize 7% como padrão.
+
+def calcular_imposto_aliquota(valor, aliquota=7):
+  imposto = valor * aliquota/100
+  return imposto
+
+for valor in valores:
+  print(f"O imposto de {valor} é {calcular_imposto_aliquota(valor)}")
+
+for valor in valores:
+  print(f"O imposto de {valor} é {calcular_imposto_aliquota(valor, 7)}")
+
+# E se agora o imposto for 10%?
   
+for valor in valores:
+  print(f"O imposto de {valor} é {calcular_imposto_aliquota(valor, 10)}")
