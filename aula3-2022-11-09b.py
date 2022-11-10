91cb3f9d9223e7f088c98160b0035d8b141c67f4
Eliel Stein <116230396+ElielStein@users.noreply.github.com>
2022-11-10 01:03:18 +0000

    função, array, laços

--- aula3-2022-11-09b.py
+++ aula3-2022-11-09b.py
@@ -10,14 +10,21 @@ preco2 = 100
 imposto2 = preco2 * 0.05
 print(imposto2)
 
-#Criar uma função calcular_imposto() que calcular um imposto de 5%
-
+#Criar uma função calcular_imposto() que calcular um imposto de 5% e retorna a quem pediu...
+#Isso é a declaração da função (Como ela funciona)
 def calcular_imposto(preco_produto):
-  imposto = preco_produto * 0.05
+  imposto = preco_produto * 0.07
   return imposto
 
+#calcular agora se fosse 7%
 
+#Aqui é o uso... aqui é imposto a calcular.. e exibir na tela
 preco = 299
 imposto = calcular_imposto(preco)
 print(imposto)
 
+valores = [1.99,24.50,78.27,1515.5]
+
+for valor in valores:
+  print(f"O imposto de {valor} é {calcular_imposto(valor)}")
+  
